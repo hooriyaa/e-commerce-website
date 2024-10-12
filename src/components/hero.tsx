72c1d627e1b58f614dc35bab7ecd87aeb0801c0d
Hooriya M. Fareed <parih3080@gmail.com>
2024-10-13 03:16:02 +0500

    push_changes

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -9,18 +9,18 @@ const Hero = () => {
     <>
       <div className="container mx-auto flex flex-col px-5  sm:flex-row items-center justify-center bg-[#F0F2F3] w-[893px] sm:w-[1100px] ">
         <div className="lg:flex-grow flex-1 md:w-1/2 lg:pr-40 sm:pl-10  flex flex-col md:text-left mb-28 items-center text-center mt-24 sm:my-0">
-          <h1 className="title-font md:mr-44 mr-4 mb-3 font-normal whitespace-nowrap text-[#272343] text-[22px] sm:text-[18px] justify-center">
+          <h1 className="title-font md:mr-44 mr-4 mb-2 font-normal whitespace-nowrap text-[#272343]  text-[22px] md:text-[20px] justify-center">
             Welcome to Chairy
           </h1>
-          <p className="title-font sm:ml-36 md:mr-44 mr-4 whitespace-nowrap text-[#272343] text-[22px] sm:text-[30px]  sm:mb-3 mb-5 leading-1 sm:text-4xl text-5xl font-bold font-[Inter] justify-center">
+          <p className="title-font sm:ml-36 md:mr-44 mr-4 whitespace-nowrap text-[#272343]  sm:text-[35px]  sm:mb-3 mb-5 leading-1 sm:text-4xl text-5xl font-bold font-[Inter] justify-center">
             Best Furniture
             <br />
             Collection for your
             <br />
             Interior
           </p>
-          <div className="sm:mr-[147px] whitespace-nowrap">
-            <button className="inline-flex text-[#FFFFFF] bg-[#029FAE] border-0 py-2 px-6 focus:outline-none hover:bg-[#02a0aede] rounded text-lg">
+          <div className="sm:mr-[147px] md:mr-[173px] whitespace-nowrap">
+            <button className="inline-flex text-[#FFFFFF] bg-[#029FAE] border-0 py-2 px-6 focus:outline-none hover:bg-[#02a0aede] rounded text-xl">
               Shop Now
               <FaArrowRightLong className="text-[#FFFFFF] text-sm mt-2 ml-5" />
             </button>
