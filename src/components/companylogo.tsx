ba104f537ba87594db04a326fb7cdf48cdd7b514
Hooriya M. Fareed <parih3080@gmail.com>
2024-10-12 21:11:05 +0500

    push_changes

--- src/components/companylogo.tsx
+++ src/components/companylogo.tsx
@@ -1,16 +1,16 @@
 import Image from "next/image";
-import logo1 from "/public/images/logo.png";
-import logo2 from "/public/images/Logo1.png";
-import logo3 from "/public/images/logo2.png";
-import logo4 from "/public/images/Logo3.png";
-import logo5 from "/public/images/logo4.png";
-import logo6 from "/public/images/logo5.png";
-import logo7 from "/public/images/logo6.png";
+import logo1 from "/public/images/Logo1.png";
+import logo2 from "/public/images/Logo2.png";
+import logo3 from "/public/images/Logo3.png";
+import logo4 from "/public/images/Logo4.png";
+import logo5 from "/public/images/Logo5.png";
+import logo6 from "/public/images/Logo6.png";
+import logo7 from "/public/images/Logo7.png";
 
 const Companylogo = () => {
   return (
     <div className="bg-white flex flex-wrap items-center justify-evenly sm:w-full md:w-[73%] mb-7 mx-auto ">
-    <div className="flex items-center justify-center ml-80 sm:ml-0 gap-10 sm:gap-14">
+    <div className="flex items-center justify-center ml-80 sm:ml-0 gap-10 sm:gap-15">
       <Image width={400} height={400} alt='logo1' src={logo1} className='h-20 w-20'/>
         <Image width={400} height={400} alt='logo1' src={logo2} className='h-20 w-20'/>
         <Image width={400} height={400} alt='logo1' src={logo3} className='h-20 w-20'/>
