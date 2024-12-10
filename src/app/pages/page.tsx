4b689fed87798bfc4621b207b2a5b4cdf3d5a06b
Hooriya M. Fareed <parih3080@gmail.com>
2024-12-10 18:07:10 +0500

    push_changes

--- src/app/pages/page.tsx
+++ src/app/pages/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Pages = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-center px-6 w-[890px]">
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-center px-6 w-[890px] sm:w-full">
       <h1 className="text-5xl font-bold text-gray-800 mb-4">Coming Soon!</h1>
       <p className="text-lg text-gray-600 mb-8">
         We are working hard to bring this feature to you. Stay tuned!
