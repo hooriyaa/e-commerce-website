d1258290623e45976908231a71b09f611387c4ee
Hooriya M. Fareed <parih3080@gmail.com>
2024-12-24 16:42:29 +0500

    push_changes

--- src/components/topCategory.tsx
+++ src/components/topCategory.tsx
@@ -6,53 +6,53 @@ import Image from "next/image";
 
 const TopCategory = () => {
   return (
-    <div className="mx-auto  text-[Inter] w-full ml-60 sm:ml-auto">
+    <div className="mx-auto text-[Inter] md:w-full w-[800px] ml-7 sm:ml-auto pb-5">
       <section className="text-gray-600 body-font">
         <div className="container mx-auto pt-14">
           <h1 className="text-3xl md:text-2xl font-semibold text-left mb-6 ml-12">
             Top Categories
           </h1>
-          <div className="flex flex-wrap  justify-center gap-4">
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-4 justify-center">
             {/* Wing Chair */}
-            <div className="w-full sm:w-[45%] md:w-[30%] p-4 hover:scale-105 duration-150">
-              <div className="relative h-56 md:h-64 rounded overflow-hidden shadow-md">
+            <div className="p-6 hover:scale-105 duration-150 group">
+            <div className="relative h-72 md:h-64 rounded overflow-hidden shadow-md">
                 <Image
                   src={product1}
                   layout="fill"
                   objectFit="cover"
                   alt="Wing Chair"
                 />
-                <div className="absolute bottom-0 left-0 w-full h-16 text-transparent hover:text-[#FFFFFF] bg-transparent hover:bg-[#000000b6] px-4 py-2 text-white">
+                <div className="absolute bottom-0 left-0 w-full h-16 hidden group-hover:block transition-all cursor-pointer bg-[#000000b6] text-[#FFFFFF] px-4 py-2">
                   <p className="text-sm font-semibold">Wing Chair</p>
                   <p className="text-xs">3,584 Products</p>
                 </div>
               </div>
             </div>
             {/* Wooden Chair */}
-            <div className="w-full sm:w-[45%] md:w-[30%] p-4 hover:scale-105 duration-150">
-              <div className="relative h-56 md:h-64 rounded overflow-hidden shadow-md">
+            <div className="p-6 hover:scale-105 duration-150 group">
+              <div className="relative h-72 md:h-64 rounded overflow-hidden shadow-md">
                 <Image
                   src={product2}
                   layout="fill"
                   objectFit="cover"
                   alt="Wooden Chair"
                 />
-                <div className="absolute bottom-0 left-0 w-full h-16 text-transparent hover:text-[#FFFFFF] bg-transparent hover:bg-[#000000b6] px-4 py-2 text-white">
+                <div className="absolute bottom-0 left-0 w-full h-16 hidden group-hover:block transition-all cursor-pointer bg-[#000000b6] text-[#FFFFFF] px-4 py-2">
                   <p className="text-sm font-semibold">Wooden Chair</p>
                   <p className="text-xs">157 Products</p>
                 </div>
               </div>
             </div>
             {/* Desk Chair */}
-            <div className="w-full sm:w-[45%] md:w-[30%] p-4 hover:scale-105 duration-150">
-              <div className="relative h-56 md:h-64 rounded overflow-hidden shadow-md">
+            <div className="p-6 hover:scale-105 duration-150 group">
+            <div className="relative h-72 md:h-64 rounded overflow-hidden shadow-md">
                 <Image
                   src={product3}
                   layout="fill"
                   objectFit="cover"
                   alt="Desk Chair"
                 />
-                <div className="absolute bottom-0 left-0 w-full h-16 text-transparent hover:text-[#FFFFFF] bg-transparent hover:bg-[#000000b6] px-4 py-2 text-white">
+                <div className="absolute bottom-0 left-0 w-full h-16 hidden group-hover:block transition-all cursor-pointer bg-[#000000b6] text-[#FFFFFF] px-4 py-2">
                   <p className="text-sm font-semibold">Desk Chair</p>
                   <p className="text-xs">154 Products</p>
                 </div>
